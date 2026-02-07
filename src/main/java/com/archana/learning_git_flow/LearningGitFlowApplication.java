f609961f24368d2265eb68b7cb446b5b9c7fb12e
Archana48 <archanananaware60@gmail.com>
2026-02-07 17:54:52 +0530

    first commit

---
+++ src/main/java/com/archana/learning_git_flow/LearningGitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.archana.learning_git_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningGitFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/archana/learning_git_flow/LearningGitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archana.learning_git_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
