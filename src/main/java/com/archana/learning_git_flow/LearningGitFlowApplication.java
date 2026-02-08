ff0825b9559c5c25ff8754a0cdc9472f31a228b0
vg-12 <vashaligupta83414@gmail.com>
2026-02-08 16:20:00 +0530

    testing

--- src/main/java/com/archana/learning_git_flow/LearningGitFlowApplication.java
+++ src/main/java/com/archana/learning_git_flow/LearningGitFlowApplication.java
@@ -10,7 +10,7 @@ public class LearningGitFlowApplication {
         SpringApplication.run(LearningGitFlowApplication.class, args);
 
         System.out.println("Hello, welcome to learning Git Flow!");
-        System.out.println("test");
+        System.out.println("testing");
     }
 
 }
